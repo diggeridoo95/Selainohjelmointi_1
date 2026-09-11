92b02097f937fbffd3c83f3e318d9f54d2002384
Hakala <Eemil.Hakala@seamk.fi>
2026-09-11 11:21:23 +0300

    Lisäys

---
+++ puhelinluettelo1/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+      id: persons.length + 1,
+    }
+
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    if (event.target.value in persons.map(person => person.name)) {
+      console.log('Name already exists')
+    }else {
+      setNewName(event.target.value)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+          <li key={person.id}>{person.name}</li>
+        )}
+      </ul>
+    </div>
+  )
+
+}
+
+export default App
